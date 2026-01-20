ba20f9b31fd48c919158e32c898b9d3623173eea
yotsufe <53263501+yotsufe@users.noreply.github.com>
2026-01-20 15:54:22 +0900

    add: 値渡し練習画面追加

---
+++ lib/data_passing_practice/data_passing_practice_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DataPassingPracticePage extends StatelessWidget {
+  const DataPassingPracticePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('値渡し')),
+      body: Column()
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_tech_research/riverpod_practice/riverpod_practice_page.dart';
+import 'data_passing_practice/data_passing_practice_page.dart';
 import 'http_practice/http_practice_page.dart';
 
 void main() {
   runApp(
-      const ProviderScope(
-        child: MyApp(),
-      ),
+    const ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
@@ -35,6 +36,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final buttonStyle = OutlinedButton.styleFrom(
+    minimumSize: const Size(double.infinity, 50),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,34 +48,52 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HttpPracticePage()),
-                );
-              },
-              child: const Text('HTTP Connection練習'),
-            ),
+              OutlinedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HttpPracticePage()),
+                  );
+                },
+                child: const Text('HTTP Connection練習'),
+              ),
 
-            const SizedBox(height: 20),
+              const SizedBox(height: 20),
 
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RiverpodPracticePage()),
-                );
-              },
-              child: const Text('Riverpod練習'),
-            ),
-          ],
+              OutlinedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RiverpodPracticePage()),
+                  );
+                },
+                child: const Text('Riverpod練習'),
+              ),
+
+              const SizedBox(height: 20),
+
+              OutlinedButton(
+                style: buttonStyle,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DataPassingPracticePage()),
+                  );
+                },
+                child: const Text('値渡し練習'),
+              ),
+            ],
+          ),
         ),
       ),
     );
